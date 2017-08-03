fd53bc4663ee84e082a24944ea167fe4e0933039
Brandon Kurtz <kurtz.brandon@gmail.com>
2017-08-03 17:12:07 -0400

    more extensible fizzbuzz

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,11 +1,30 @@
-def fizzbuzz(num)
-  string = ''
-  string += 'fizz' if (num % 3).zero?
-  string += 'buzz' if (num % 5).zero?
-  string = num if string == '' # if num isn't divisible by 3 or 5
-  string
+def game_rules
+  rules = {
+    3 => { text: 'Fizz' },
+    5 => { text: 'buzz' },
+    7 => { text: 'pop' },
+    11 => { text: 'crack' }
+  }
+
+  # [7].each do |num|
+  #   rules[num][:override] = true
+  # end
+
+  rules
 end
 
+def fizzbuzz_override(num, rules)
+  output = ''
+  rules.each do |divisor, props|
+    if (num % divisor).zero?
+      props[:override] ? output = props[:text] : output += props[:text]
+    end
+  end
+  output = num if output == ''
+  output
+end
+
+rules = game_rules
 (1..30).each do |num|
-  puts fizzbuzz(num)
+  puts fizzbuzz_override(num, rules)
 end
